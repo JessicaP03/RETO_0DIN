f811ac21fb6244df80d204d2db35dc1358180a65
jessica <2dam@HZ375754.TartangaLH.eus>
2022-09-28 12:16:09 +0200

    ventana swing realizada

--- src/reto_0_din/view/ViewFactory.java
+++ src/reto_0_din/view/ViewFactory.java
@@ -18,26 +18,25 @@ public class ViewFactory {
     
     private ResourceBundle data;
     private final String text_type = "TEXT";
-   
+    private final String swing_type = "SWING";
+    private final String FX_TYPE = "FX";
     
 
     public View getView() {
-        View view_t = null;
-        View view_swing = null;
-        View view_fx;
+        View view = null;
 
        String data=ResourceBundle.getBundle("RETO_0_DIN.TipoEjecucion").getString("view_type"); //Elige el tipo de vista que quiere utilizar, file o bd.
         if (text_type.equals(data) ){ 
             
-              view_t = new  ViewFileImplementation(); 
+              view = new ViewFileImplementation(); 
               
-        }else if (text_type.equals(data)){
-              view_t = (View) new  ViewSwingImplementation(); 
+        }else if (swing_type.equalsIgnoreCase(data)){
+              view = new ViewSwingImplementation(); 
            
         }else {
-                view_fx = (View) new ViewFXImplementation();
+             //   view =  (View) new ViewFXImplementation();
                 }
-        return view_t;
+        return view;
 
 }
     
